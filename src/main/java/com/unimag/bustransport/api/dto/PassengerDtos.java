04dd5ae5447c32dfd7d67a2d55a75aea53e48ac7
Jose Alejandro <214848445+JoseRodriguez0001@users.noreply.github.com>
2025-11-05 14:10:03 -0500

    fix(dtos): arreglos en dtos

--- src/main/java/com/unimag/bustransport/api/dto/PassengerDtos.java
+++ src/main/java/com/unimag/bustransport/api/dto/PassengerDtos.java
@@ -11,23 +11,25 @@ import java.time.OffsetDateTime;
 
 public class PassengerDtos {
     public record PassengerCreateRequest(
-            @NotBlank String fullname,
+            @NotBlank String fullName,
             @NotBlank String documentType,
             @NotBlank String documentNumber,
             @Past
             LocalDate birthDate,
             @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "El teléfono debe tener entre 10 y 15 dígitos")
-            @NotBlank String phoneNumber
+            @NotBlank String phoneNumber,
+            @Nullable
+            Long userId
     ) implements Serializable {}
     public record PassengerUpdateRequest(
-            String fullname,
+            String fullName,
             String documentType,
             String documentNumber,
             LocalDate birthDate,
             String phoneNumber) implements Serializable {}
     public record PassengerResponse(
             Long id,
-            String fullname,
+            String fullName,
             String documentType,
             String documentNumber,
             LocalDate birthDate,
