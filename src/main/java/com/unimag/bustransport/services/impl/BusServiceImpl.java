07c8c19091b74020d8761c72437b5e0e4ba14814
Jose Alejandro <214848445+JoseRodriguez0001@users.noreply.github.com>
2025-11-06 12:00:09 -0500

    feat(ServicesImpl): implmentacion de Servicio

--- src/main/java/com/unimag/bustransport/services/impl/BusServiceImpl.java
+++ src/main/java/com/unimag/bustransport/services/impl/BusServiceImpl.java
@@ -2,38 +2,118 @@ package com.unimag.bustransport.services.impl;
 
 import com.unimag.bustransport.api.dto.BusDtos;
 import com.unimag.bustransport.api.dto.SeatDtos;
+import com.unimag.bustransport.domain.entities.Bus;
+import com.unimag.bustransport.domain.entities.Trip;
+import com.unimag.bustransport.domain.repositories.BusRepository;
+import com.unimag.bustransport.exception.DuplicateResourceException;
+import com.unimag.bustransport.exception.NotFoundException;
 import com.unimag.bustransport.services.BusService;
+import com.unimag.bustransport.services.mapper.BusMapper;
+import com.unimag.bustransport.services.mapper.SeatMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class BusServiceImpl implements BusService {
+
+    private final BusRepository repository;
+    private final BusMapper mapper;
+    private final SeatMapper seatMapper;
     @Override
     public BusDtos.BusResponse createBus(BusDtos.BusCreateRequest request) {
-        return null;
+        // Validar que la placa sea única
+        repository.findByPlate(request.plate()).ifPresent(existingBus -> {
+            log.warn("Intento de crear bus con placa duplicada: {}", request.plate());
+            throw new DuplicateResourceException(
+                    String.format("Ya existe un bus con la placa '%s'", request.plate()));
+
+        });
+
+
+        Bus bus = mapper.toEntity(request);
+
+        // Guardar bus
+        Bus savedBus = repository.save(bus);
+        log.info("Bus creado exitosamente con ID: {} y placa: {}", savedBus.getId(), savedBus.getPlate());
+
+        return mapper.toResponse(savedBus);
     }
 
     @Override
     public void updateBus(Long id, BusDtos.BusUpdateRequest request) {
+        Bus bus = repository.findById(id).orElseThrow(
+                ()-> new NotFoundException("Bus not found")
+        );
 
+        mapper.updateEntityFromRequest(request,bus);
+        repository.save(bus);
     }
 
     @Override
     public void deleteBus(Long id) {
+        Bus bus = repository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Bus not found"));
 
+        boolean hasActiveTrip = bus.getTrips().stream()
+                .anyMatch(trip ->
+                                trip.getStatus() == Trip.Status.SCHEDULED ||
+                                trip.getStatus() == Trip.Status.BOARDING ||
+                                trip.getStatus() == Trip.Status.DEPARTED
+
+                );
+
+        if (hasActiveTrip) {
+            throw new IllegalArgumentException("Cannot delete bus: it is currently assigned to an active trip");
+        }
+        //cambiamos estado
+        bus.setStatus(Bus.Status.RETIRED);
+        repository.save(bus);
     }
 
     @Override
     public BusDtos.BusResponse getBus(Long id) {
-        return null;
+        Bus bus = repository.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Bus no encontrado con ID: {}", id);
+                    return new NotFoundException(
+                            String.format("Bus con ID %d no encontrado", id)
+                    );
+                });
+
+        return mapper.toResponse(bus);
     }
 
     @Override
     public List<BusDtos.BusResponse> getAllBus() {
-        return List.of();
+        List<Bus> buses = repository.findAll();
+
+        return buses.stream()
+                .map(mapper::toResponse)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<SeatDtos.SeatResponse> getAllSeatsByBusId(Long id) {
-        return List.of();
+        Bus bus = repository.findByIdWithSeats(id)
+                .orElseThrow(() -> {
+                    log.error("Bus no encontrado con ID: {}", id);
+                    return new NotFoundException(
+                            String.format("Bus con ID %d no encontrado", id)
+                    );
+                });
+
+        List<SeatDtos.SeatResponse> seats = bus.getSeats().stream()
+                .map(seatMapper::toResponse)
+                .collect(Collectors.toList());
+
+        return seats;
     }
 }
--- src/main/java/com/unimag/bustransport/services/impl/TicketServiceImpl.java
+++ src/main/java/com/unimag/bustransport/services/impl/TicketServiceImpl.java
@@ -1,4 +1,65 @@
 package com.unimag.bustransport.services.impl;
 
-public class TicketServiceImpl {
+import com.unimag.bustransport.api.dto.TicketDtos;
+import com.unimag.bustransport.services.TicketService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TicketServiceImpl implements TicketService {
+    @Override
+    public TicketDtos.TicketResponse createTicket(TicketDtos.TicketCreateRequest request) {
+        return null;
+    }
+
+    @Override
+    public void deleteTicket(Long id) {
+
+    }
+
+    @Override
+    public TicketDtos.TicketResponse getTicket(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<TicketDtos.TicketResponse> getTicketsByTrip(Long tripId) {
+        return List.of();
+    }
+
+    @Override
+    public List<TicketDtos.TicketResponse> getTicketsByPurchase(Long purchaseId) {
+        return List.of();
+    }
+
+    @Override
+    public List<TicketDtos.TicketResponse> getTicketsByPassenger(Long passengerId) {
+        return List.of();
+    }
+
+    @Override
+    public TicketDtos.TicketResponse assignPassenger(Long ticketId, Long passengerId) {
+        return null;
+    }
+
+    @Override
+    public void markCheckedIn(Long ticketId) {
+
+    }
+
+    @Override
+    public void generateQrForTicket(Long ticketId) {
+
+    }
+
+    @Override
+    public void validateQrForTicket(String qrCode) {
+
+    }
 }
