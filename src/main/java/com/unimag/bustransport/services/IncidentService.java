d90507b79045299de574211de9820146f0bb3491
Jose Alejandro <214848445+JoseRodriguez0001@users.noreply.github.com>
2025-11-06 12:04:58 -0500

    feat(services): crea firmas

--- src/main/java/com/unimag/bustransport/services/IncidentService.java
+++ src/main/java/com/unimag/bustransport/services/IncidentService.java
@@ -1,4 +1,14 @@
 package com.unimag.bustransport.services;
 
+import com.unimag.bustransport.api.dto.IncidentDtos;
+import com.unimag.bustransport.domain.entities.Incident;
+
+import java.util.List;
+
 public interface IncidentService {
+    IncidentDtos.IncidentResponse createIncident(IncidentDtos.IncidentCreateRequest request);
+    void updateIncident(Long incidentId,IncidentDtos.IncidentUpdateRequest request);
+    void deleteIncident(Long incidentId);
+    List<IncidentDtos.IncidentResponse> findByIncidentByType(Incident.EntityType type);
+    List<IncidentDtos.IncidentResponse> findIncidentsRecentByType(Incident.Type type);
 }
