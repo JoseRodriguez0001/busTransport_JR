bbe2aa2c54d6e041e4a0e1dc8cd835bd23d69e76
Jose Alejandro <214848445+JoseRodriguez0001@users.noreply.github.com>
2025-11-06 12:05:15 -0500

    feat(services): crea firmas

--- src/main/java/com/unimag/bustransport/services/KpiService.java
+++ src/main/java/com/unimag/bustransport/services/KpiService.java
@@ -1,4 +1,13 @@
 package com.unimag.bustransport.services;
 
+import com.unimag.bustransport.api.dto.KpiDtos;
+
+import java.util.List;
+
 public interface KpiService {
+    KpiDtos.KpiResponse createKpi(KpiDtos.KpiCreateRequest request);
+    void updateKpi(Long kpiId,KpiDtos.KpiUpdateRequest request);
+    void deleteKpi(Long kpiId);
+    KpiDtos.KpiResponse getKpiByName(String kpiName);
+    List<KpiDtos.KpiResponse> getKpiByRecentMetrics();
 }
